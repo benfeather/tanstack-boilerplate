0d3c7af790e43d94d6416585f1e25d2ee86fa402
Ben Feather <contact@benfeather.dev>
2025-12-06 17:01:01 +1300

    WIP

--- apps/mobile/src/routeTree.gen.ts
+++ apps/mobile/src/routeTree.gen.ts
@@ -9,9 +9,9 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as LoginRouteImport } from './routes/login'
 import { Route as DashboardRouteImport } from './routes/dashboard'
 import { Route as IndexRouteImport } from './routes/index'
-import { Route as LoginRouteImport } from './routes/login'
 
 const LoginRoute = LoginRouteImport.update({
   id: '/login',
