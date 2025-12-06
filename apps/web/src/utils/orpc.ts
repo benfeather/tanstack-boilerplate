7f8e566a7d05dfa595b46ebeae03148504860fe1
Ben Feather <contact@benfeather.dev>
2025-12-06 13:04:09 +1300

    Add web project

---
+++ apps/web/src/utils/orpc.ts
@@ -0,0 +1,51 @@
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import type { RouterClient } from "@orpc/server";
+import { createRouterClient } from "@orpc/server";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { createIsomorphicFn } from "@tanstack/react-start";
+import { createContext } from "@workspace/api/context";
+import { appRouter } from "@workspace/api/routers/index";
+import { toast } from "sonner";
+
+export const queryClient = new QueryClient({
+	queryCache: new QueryCache({
+		onError: (error) => {
+			toast.error(`Error: ${error.message}`, {
+				action: {
+					label: "retry",
+					onClick: () => {
+						queryClient.invalidateQueries();
+					},
+				},
+			});
+		},
+	}),
+});
+
+const getORPCClient = createIsomorphicFn()
+	.server(() =>
+		createRouterClient(appRouter, {
+			context: async ({ req }) => {
+				return createContext({ context: req });
+			},
+		}),
+	)
+	.client((): RouterClient<typeof appRouter> => {
+		const link = new RPCLink({
+			url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
+			fetch(url, options) {
+				return fetch(url, {
+					...options,
+					credentials: "include",
+				});
+			},
+		});
+
+		return createORPCClient(link);
+	});
+
+export const client: RouterClient<typeof appRouter> = getORPCClient();
+
+export const orpc = createTanstackQueryUtils(client);
